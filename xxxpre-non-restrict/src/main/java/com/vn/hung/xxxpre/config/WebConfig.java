beebb59f00db035878a630e0a2271e970d9cd68d
vnlevh <hungglevan@gmail.com>
2025-11-18 03:14:09 -0500

    push

--- xxxpre-non-restrict/src/main/java/com/vn/hung/xxxpre/config/WebConfig.java
+++ xxxpre-non-restrict/src/main/java/com/vn/hung/xxxpre/config/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Apply CORS to all paths starting with /api/
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all standard methods, including OPTIONS
-                .allowedHeaders("*") // Allow all headers
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
-                .maxAge(3600); // Cache pre-flight response for 1 hour
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
+
 }
